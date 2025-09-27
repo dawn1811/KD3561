9a8de54d5b577f611364bea4368369e3b55dae2f
dawn1811 <heather.vaughn87@outlook.com>
2025-09-27 08:15:54 -0500

    Add files via upload

---
+++ frontend/public/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+});
+
+const messaging = firebase.messaging();
